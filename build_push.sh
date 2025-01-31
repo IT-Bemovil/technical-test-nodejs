4c693d7979a2355003a589fb670df8c4abef2d52
andycr95 <andycr95@icloud.com>
2025-01-30 21:05:00 -0500

    updating build script to docker-compose

--- build_push.sh
+++ build_push.sh
@@ -5,13 +5,13 @@ REPOSITORY_URL=$(terraform output -raw ecr_repository_url)
 IMAGE_TAG="latest"
 
 # Construir la imagen Docker
-pnpm run docker:build
+docker-compose build todo_app
 
 # Iniciar sesión en ECR
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $REPOSITORY_URL
 
 # Etiquetar la imagen
-docker tag myapp:latest $REPOSITORY_URL:$IMAGE_TAG
+docker tag technical-test-nodejs_todo_app:latest $REPOSITORY_URL:$IMAGE_TAG
 
 # Subir la imagen a ECR
 docker push $REPOSITORY_URL:$IMAGE_TAG
