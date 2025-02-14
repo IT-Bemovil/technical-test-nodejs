94e2c7aa25f62e3eb6f33e7f7bdab75956254620
juanValencia88 <juan.valencia@bitsoluciones.com.co>
2025-02-14 16:18:07 -0500

    refactorizacion de controlador
    separacion de logica de negocio  y controlador
    
    controlador solo encargado de peticiopnes http y sevices dde la logica de negocio

--- src/controllers/login.js
+++ src/controllers/login.js
@@ -1,38 +1,13 @@
-require('dotenv').config();
-const { User } = require('../db')
+const loginService = require("../services/login");
 
-const jwt = require('jsonwebtoken');
 
 
 const login = async (req, res) => {
+
     try{
-
-        const { JWT_SECRET } = process.env
-
-        const secret = JWT_SECRET 
-
         const { email, password } = req.body;
-        if(!email ||!password){
-            return res.status(400).json({ msg: 'Ingresar todos los parametros' });
-        }
-        
-        const user = await User.findOne({
-            where: { email, password }
-          });
-
-          if (!user) {
-            return res.status(401).json({ msg: 'Usuario o contraseña incorrectos' });
-          }
-
-          const payload = {
-            email : user.email,
-            password : user.password
-          };
-          //firmo el token
-          const token = jwt.sign(payload, secret);
-    
-          return res.status(200).json({ payload, token });
-          
+        const response = await loginService(email, password);
+        return res.status(200).json(response);
 
     }catch(e){
         console.error(e);
--- src/services/login.js
+++ src/services/login.js
@@ -1,8 +1,41 @@
 
 
 
-const loginService = async (data) => {
+require('dotenv').config();
+const { User } = require('../db')
+
+const jwt = require('jsonwebtoken');
+
+
+const loginService = async (email, password) => {
     
-};
+
+        const { JWT_SECRET } = process.env
+
+        const secret = JWT_SECRET 
+
+        if(!email ||!password){
+            return res.status(400).json({ msg: 'Ingresar todos los parametros' });
+        }
+        
+        const user = await User.findOne({
+            where: { email, password }
+          });
+
+          if (!user) {
+            return { msg: 'Usuario o contraseña incorrectos' };
+          }
+
+          const payload = {
+            email : user.email,
+            password : user.password
+          };
+          //firmo el token
+          const token = jwt.sign(payload, secret);
+    
+          return { payload, token };
+          
+
+}
 
 module.exports = loginService;
