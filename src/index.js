91355c6510e710a8e617c411eb6d55dcb2fb4569
juanValencia88 <juan.valencia@bitsoluciones.com.co>
2025-02-14 14:49:09 -0500

    conexion instancia sequelize a postgres

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 require("dotenv").config();
 
 const app = require("./app");
-// const { conn } = require("../src/config/db") ;
+const { conn } = require("../src/config/db") ;
 
 
 const PORT = process.env.PORT ;
 
 
-// conn.sync({ force: false }).then(() => {
+conn.sync({ force: false }).then(() => {
     app.listen(PORT, '0.0.0.0', () => {
       console.log(`%s listening at ${PORT}`);
     });
-// });
+});
