8e4043a7496ef1c45334d34909e2f0fc9181d6fd
juanValencia88 <juan.valencia@bitsoluciones.com.co>
2025-02-14 16:37:39 -0500

    controlador y servicio de crear usuario
    implenmentacion middleware de vereificacion token jwt

--- src/routes/userRoutes/index.js
+++ src/routes/userRoutes/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const login = require('../../controllers/login');
 const { verifyToken } = require('../../middlewares/auth');
+const postUser = require('../../controllers/postUser');
 
 const userRoutes = express.Router();
 
 
 userRoutes.post('/auth/login', login);
+userRoutes.post('/auth/register', verifyToken, postUser);
 
 
 module.exports = userRoutes;
---
+++ src/services/postUserService.js
@@ -0,0 +1,20 @@
+const { User } = require('../db');
+
+
+const postUserService = async (email, password) => {
+    if(!email || !password){
+        return 'email y password son requeridos'
+    }
+    const user = await User.findOne({ where: { email } });
+
+    if(user){
+        console.log(user)
+        return 'El usuario ya existe';
+    }else{
+        const newUser =  await User.create({ email, password });
+        await newUser.save();
+        return {newUser};
+    }
+};
+
+module.exports = postUserService;
